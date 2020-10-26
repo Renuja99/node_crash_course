38d2e21e4450cf272dd34d42d35415ddf3d1e764
Renuja De Costa <devmindadeco@gmail.com>
2020-10-26 15:02:05 +0530

    Introduction to streams and reading Streams

---
+++ streams.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+const readStream = fs.createReadStream('./docs/blog3.txt', {encoding:'utf8'});
+
+readStream.on('data', (chunk)=> {
+
+    console.log('--------NEW CHUNK--------');
+    console.log(chunk);
+
+});
+
+//? .on is an event listener on readStream that listens to a 'data' event . Everytime we receive a Buffer of data from the Sream(that is readily available for use) , we fire the callback function and we get access to that chunk of data inside the function,
+
+
+//? the fs module createReadStream is passed two arguments. the first argument is going to say where we pass data from to the stream and where we want to read data from . The second argument converts the chunk of data into a readable format.
