ae0fbdb03bd048a7d5b7bbbe89d3e52e749d6501
Renuja De Costa <devmindadeco@gmail.com>
2020-10-26 15:23:57 +0530

    Piping from readStream to the writeStream

--- streams.js
+++ streams.js
@@ -2,12 +2,12 @@ const fs = require('fs');
 
 const readStream = fs.createReadStream('./docs/blog3.txt', {encoding:'utf8'});
 
-readStream.on('data', (chunk)=> {
+// readStream.on('data', (chunk)=> {
 
-    console.log('--------NEW CHUNK--------');
-    console.log(chunk);
+//     console.log('--------NEW CHUNK--------');
+//     console.log(chunk);
 
-});
+// });
 
 //? .on is an event listener on readStream that listens to a 'data' event . Everytime we receive a Buffer of data from the Sream(that is readily available for use) , we fire the callback function and we get access to that chunk of data inside the function,
 
@@ -17,8 +17,10 @@ readStream.on('data', (chunk)=> {
 
 const writeStream = fs.createWriteStream('./docs/blog4.txt');
 
-readStream.on('data', (chunk)=>{
+// readStream.on('data', (chunk)=>{
 
-    writeStream.write('\n----NEW CHUNK---\n');
-    writeStream.write(chunk);
-});
+//     writeStream.write('\n----NEW CHUNK---\n');
+//     writeStream.write(chunk);
+// });
+
+readStream.pipe(writeStream);
