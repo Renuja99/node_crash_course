39f3c03c11b7c92048d385a198c42ca9aef736a1
Renuja De Costa <devmindadeco@gmail.com>
2020-10-26 13:09:06 +0530

    Create and delete directory

--- files.js
+++ files.js
@@ -2,40 +2,64 @@
 
 const fs = require('fs');
 
-//reading files 
+// //reading files 
 
-//? the readFile method is asynchronous
-fs.readFile('./docs/blog1.txt', (err, data) =>{
+// //? the readFile method is asynchronous
+// fs.readFile('./docs/blog1.txt', (err, data) =>{
 
-    if(err){
-        console.log(err);
-    }
+//     if(err){
+//         console.log(err);
+//     }
 
-    console.log(data.toString());
-});
+//     console.log(data.toString());
+// });
 
-console.log("hello");
+// console.log("hello");
 
 
 //writing files
 
 // ***__________re-writing on an existing file_________***
 
-fs.writeFile('./docs/blog1.txt', 'hello, world' , ()=>{
+// fs.writeFile('./docs/blog1.txt', 'hello, world' , ()=>{
 
-    console.log('file was written');
-})
+//     console.log('file was written');
+// });
 
 
 // **___________creates a new file and writes the given string
 
-fs.writeFile('./docs/blog1.txt', 'hello, world' , ()=>{
+// fs.writeFile('./docs/blog2.txt', 'hello, world' , ()=>{
 
-    console.log('file was written');
-})
+//     console.log('file2 was written');
+// });
 
 // directories
 
+// ? Creates directory if it doesnt exist
+if(!fs.existsSync('./assets'))
+{fs.mkdir('./assets', (error)=>{
+
+    if(error){
+        console.log(error);
+    }
+
+    console.log('folder created');
+ })
+}
+
+
+//? deletes directory if it deos exist
+else{
+    fs.rmdir('./assets', (error)=>{
+
+        if(error){
+            console.log(error);
+        }
+
+        console.log('folder deleted');
+    })
+}
 
 
 //deleting files
