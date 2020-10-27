107933ba2652905a14141c838c0152566c158b17
Renuja De Costa <devmindadeco@gmail.com>
2020-10-27 10:30:11 +0530

    Sending text as a response to the browser

--- server.js
+++ server.js
@@ -2,11 +2,17 @@
 
  const server = http.createServer((req, res)=>{
      
-    console.log('A request is made..');
+    console.log(req.url , req.method);
 
-    //
+    // set header content type
+    //? The response headers give more information to the browser about what kind of response is coming back to it.
+
+    res.setHeader('Content-type', 'text/plain' );
+    res.write('hello, Ninjas');
+    res.end();
  });
-//? createServer takes a callback function as an argument and runs everytime a request comes in to our server. Inside this function we get access to the request object and the response object.
+
+ //? createServer takes a callback function as an argument and runs everytime a request comes in to our server. Inside this function we get access to the request object and the response object.
 
 
  //? ------------denotes that the local server is listening on port 3000 and a request is sent to this 
