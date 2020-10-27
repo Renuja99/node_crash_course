f335f86c93e736233943698cb8fb4db91f171d2c
Renuja De Costa <devmindadeco@gmail.com>
2020-10-27 10:49:13 +0530

    Return an html page as a response

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
  const http = require('http');
+ const fs  = require('fs');
 
  const server = http.createServer((req, res)=>{
      
@@ -7,14 +8,27 @@
     // set header content type
     //? The response headers give more information to the browser about what kind of response is coming back to it.
 
-    res.setHeader('Content-type', 'text/html' );
-    res.write('<p>Hello Ninjas</p>');
-    res.write('<p>Hello again Ninjas</p>');
-    res.end();
+   //  res.setHeader('Content-type', 'text/html' );
+   //  res.write('<p>Hello Ninjas</p>');
+   //  res.write('<p>Hello again Ninjas</p>');
+   //  res.end();
 
     //? First we have got to specify the type of content we are going to send as a response.
     //? Secondly we write what we are going to return 
     //? Finally we end the response.
+
+    //sending an html file
+
+    fs.readFile('./views/index.html', (err, data)=>{
+       if(err){
+          console.log(err);
+          res.end();
+       }else{
+          res.write(data);
+          res.end();
+       }
+
+    })
  });
 
  //? createServer takes a callback function as an argument and runs everytime a request comes in to our server. Inside this function we get access to the request object and the response object.
@@ -24,7 +38,7 @@
 
  server.listen(3000, 'localhost', ()=>{
 
-   console.log('Listening for requests on port 3000 ');
+   console.log('Listening for requests on port 3000....');
  });
 
  
