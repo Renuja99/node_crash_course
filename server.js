a48f8416a474cf6e2110f144e36ef0cc8cdcde61
Renuja De Costa <devmindadeco@gmail.com>
2020-10-27 10:33:01 +0530

    Sending html as a response to the browser

--- server.js
+++ server.js
@@ -7,9 +7,14 @@
     // set header content type
     //? The response headers give more information to the browser about what kind of response is coming back to it.
 
-    res.setHeader('Content-type', 'text/plain' );
-    res.write('hello, Ninjas');
+    res.setHeader('Content-type', 'text/html' );
+    res.write('<p>Hello Ninjas</p>');
+    res.write('<p>Hello again Ninjas</p>');
     res.end();
+
+    //? First we have got to specify the type of content we are going to send as a response.
+    //? Secondly we write what we are going to return 
+    //? Finally we end the response.
  });
 
  //? createServer takes a callback function as an argument and runs everytime a request comes in to our server. Inside this function we get access to the request object and the response object.
